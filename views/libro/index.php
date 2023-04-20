320184badd30b04ffe33b5ab1f1846142332b3b1
JoaquinMaidana <jocomaidana11@gmail.com>
2023-04-20 16:44:22 -0300

    Creados los controladores y directorios para las vistas

---
+++ views/libro/index.php
@@ -0,0 +1,22 @@
+<?php
+
+use app\models\Libro;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/** @var yii\web\View $this */
+
+$this->title = 'Libros';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="libro-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+
+    
+
+
+</div>
