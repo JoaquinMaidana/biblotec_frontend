1da35500e177294034c2c68bea1ca2105ac436bf
JoaquinMaidana <jocomaidana11@gmail.com>
2023-04-21 20:19:00 -0300

    tratando de ajustar las datatables

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -11,6 +11,9 @@ use yii\bootstrap5\Nav;
 use yii\bootstrap5\NavBar;
 
 AppAsset::register($this);
+$this->registerCssFile('https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css');
+
+$this->registerJsFile('https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js', ['depends' => [\yii\web\JqueryAsset::class]]);
 
 $this->registerCsrfMetaTags();
 $this->registerMetaTag(['charset' => Yii::$app->charset], 'charset');
@@ -18,6 +21,7 @@ $this->registerMetaTag(['name' => 'viewport', 'content' => 'width=device-width, 
 $this->registerMetaTag(['name' => 'description', 'content' => $this->params['meta_description'] ?? '']);
 $this->registerMetaTag(['name' => 'keywords', 'content' => $this->params['meta_keywords'] ?? '']);
 $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii::getAlias('@web/favicon.ico')]);
+
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
--- views/libro/index.php
+++ views/libro/index.php
@@ -15,8 +15,33 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-
+    <table id="myTable" >
+    <thead>
+        <tr>
+            <th>Column 1</th>
+            <th>Column 2</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Row 1 Data 1</td>
+            <td>Row 1 Data 2</td>
+        </tr>
+        <tr>
+            <td>Row 2 Data 1</td>
+            <td>Row 2 Data 2</td>
+        </tr>
+    </tbody>
+</table>
     
 
 
 </div>
+
+
+
+<script>
+    $(document).ready( function () {
+    $('#myTable').DataTable();
+} );
+</script>
