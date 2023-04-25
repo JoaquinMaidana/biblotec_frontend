465b1df07e84df4485024fc514bf53101d232a99
JoaquinMaidana <jocomaidana11@gmail.com>
2023-04-25 15:33:24 -0300

    boton de inicio

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -28,7 +28,7 @@ use yii\helpers\Url;
 
     <header id="header">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="<?= Url::toRoute(['site/index']); ?>">Inicio</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
