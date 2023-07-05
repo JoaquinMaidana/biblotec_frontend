8336c475011de078bdf6fcea378e0290c35d4bad
L-A-2003 <luciano.avondet@estudiantes.utec.com.uy>
2023-07-04 23:15:08 -0300

    prueba3

--- views/libro/crearLibro.php
+++ views/libro/crearLibro.php
@@ -22,9 +22,9 @@ if (isset($categorias)) {
   }
 
   label,
-    p {
-        color: white;
-    }
+  p {
+    color: white;
+  }
 </style>
 
 <?php if (isset($libro) && !empty($libro) && $isGet === 'No') : ?>
@@ -75,109 +75,83 @@ if (isset($categorias)) {
 
   <div class="collapse" id="collapseScanner">
     <div class="controls">
-      <!--<fieldset class="input-group">
+      <fieldset class="input-group">
         <button class="stop">Stop</button>
-      </fieldset>-->
+      </fieldset>
       <fieldset class="reader-config-group">
-        <hr>
-        <div class="row">
-          <div class="col-3 text-end">
-            <label>Tipo de barcode:</label>
-          </div>
-          <div class="col">
-            <select name="decoder_readers" class="form-control">
-              <option value="code_128">Code 128</option>
-              <option value="code_39">Code 39</option>
-              <option value="code_39_vin">Code 39 VIN</option>
-              <option value="ean" selected="selected">EAN</option>
-              <option value="ean_extended">EAN-extended</option>
-              <option value="ean_8">EAN-8</option>
-              <option value="upc">UPC</option>
-              <option value="upc_e">UPC-E</option>
-              <option value="codabar">Codabar</option>
-              <option value="i2of5">Interleaved 2 of 5</option>
-              <option value="2of5">Standard 2 of 5</option>
-              <option value="code_93">Code 93</option>
-            </select>
-          </div>
-        </div>
-
-        <div class="row mt-3">
-          <div class="col-3 text-end">
-            <label>Resolución (ancho):</label>
-          </div>
-          <div class="col">
-            <select name="input-stream_constraints" class="form-control">
-              <option value="320x240">320px</option>
-              <option value="640x480">640px</option>
-              <option value="800x600">800px</option>
-              <option selected="selected" value="1280x720">1280px</option>
-              <option value="1600x960">1600px</option>
-              <option value="1920x1080">1920px</option>
-            </select>
-          </div>
-        </div>
-
-        <div class="row mt-3">
-          <div class="col-3 text-end">
-            <label>Tamaño del parche:</label>
-          </div>
-          <div class="col">
-            <select name="locator_patch-size" class="form-control">
-              <option value="x-small">Muy chico</option>
-              <option value="small">Chico</option>
-              <option selected="selected" value="medium">Mediano</option>
-              <option value="large">Grande</option>
-              <option value="x-large">Muy grande</option>
-            </select>
-          </div>
-        </div>
-
-        <div class="row mt-3">
-          <div class="col-3 text-end">
-            <label>Trabajadores:</label>
-          </div>
-          <div class="col">
-            <select name="numOfWorkers" class="form-control">
-              <option value="0">0</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option selected="selected" value="4">4</option>
-              <option value="8">8</option>
-            </select>
-          </div>
-        </div>
-
-        <div class="row mt-3">
-          <div class="col-3 text-end">
-            <label>Cámara:</label>
-          </div>
-          <div class="col">
-            <select name=" input-stream_constraints" id="deviceSelection" class="form-control">
-            </select>
-          </div>
-        </div>
-
-        <div class="row mt-3">
-          <div class="col-3 text-end">
-            <label>Media muestra:</label>
-          </div>
-          <div class="col">
-            <input type="checkbox" checked="checked" name="locator_half-sample" class="form-check-input" />
-          </div>
-        </div>
-
+        <label>
+          <span>Barcode-Type</span>
+          <select name="decoder_readers">
+            <option value="code_128">Code 128</option>
+            <option value="code_39">Code 39</option>
+            <option value="code_39_vin">Code 39 VIN</option>
+            <option value="ean" selected="selected">EAN</option>
+            <option value="ean_extended">EAN-extended</option>
+            <option value="ean_8">EAN-8</option>
+            <option value="upc">UPC</option>
+            <option value="upc_e">UPC-E</option>
+            <option value="codabar">Codabar</option>
+            <option value="i2of5">Interleaved 2 of 5</option>
+            <option value="2of5">Standard 2 of 5</option>
+            <option value="code_93">Code 93</option>
+          </select>
+        </label>
+        <label>
+          <span>Resolution (width)</span>
+          <select name="input-stream_constraints">
+            <option value="320x240">320px</option>
+            <option value="640x480">640px</option>
+            <option value="800x600">800px</option>
+            <option selected="selected" value="1280x720">1280px</option>
+            <option value="1600x960">1600px</option>
+            <option value="1920x1080">1920px</option>
+          </select>
+        </label>
+        <label>
+          <span>Patch-Size</span>
+          <select name="locator_patch-size">
+            <option value="x-small">x-small</option>
+            <option value="small">small</option>
+            <option selected="selected" value="medium">medium</option>
+            <option value="large">large</option>
+            <option value="x-large">x-large</option>
+          </select>
+        </label>
+        <label>
+          <span>Half-Sample</span>
+          <input type="checkbox" checked="checked" name="locator_half-sample" />
+        </label>
+        <label>
+          <span>Workers</span>
+          <select name="numOfWorkers">
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option selected="selected" value="4">4</option>
+            <option value="8">8</option>
+          </select>
+        </label>
+        <label>
+          <span>Camera</span>
+          <select name="input-stream_constraints" id="deviceSelection">
+          </select>
+        </label>
         <label style="display: none">
-          <span>Zoom:</span>
+          <span>Zoom</span>
           <select name="settings_zoom"></select>
         </label>
         <label style="display: none">
-          <span>Torch:</span>
+          <span>Torch</span>
           <input type="checkbox" name="settings_torch" />
         </label>
       </fieldset>
     </div>
+    <div id="result_strip">
+      <ul class="thumbnails"></ul>
+      <ul class="collector"></ul>
+    </div>
     <div id="interactive" class="viewport"></div>
+    </section>
   </div>
 
   <hr>
